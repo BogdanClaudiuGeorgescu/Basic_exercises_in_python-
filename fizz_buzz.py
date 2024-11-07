893294627f448a9f41ac5b60a15bdb747cf0be86
Bogdan Georgescu <georgescubogdan88@gmail.com>
2024-11-07 15:44:56 +0200

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,7 +3,7 @@
 # For each multiple of 5, print "Buzz" instead of the number. For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 
 
-# The solution:
+# Solution 1: FizzBuzz with a hardcoded fixed range 
 for number in range (1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
@@ -13,3 +13,26 @@ for number in range (1, 101):
         print("Buzz")
     else:
         print(number)
+
+
+# Solution 2: FizzBuzz with a dinamic range 
+user_input_minimum = input("What is the minimum number you want for the range to to capped at? ")
+user_input_maximum = input("What is the maximum number you want for the range to to capped at? ")
+
+def test():
+    user_input_minimum = input("What is the minimum number you want for the range to to capped at? ")
+    user_input_maximum = input("What is the maximum number you want for the range to to capped at? ")
+
+if user_input_maximum < user_input_minimum:
+    print("The minimum number needs to be smaller by at least 10 than the maximum number. Try again! ")
+    test()
+else:
+    for number in range (int(user_input_minimum), int(user_input_maximum)):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
